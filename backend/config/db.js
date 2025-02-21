cdba301c44f6086e5e1bdd9a6dab0bff26afe164
Marcelo <marcelomarquesribeiro1@gmail.com>
2025-02-21 16:43:13 -0300

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,16 +2,11 @@ const Sequelize = require('sequelize')
 require("dotenv").config(); // Carregar variáveis de ambiente
 
 // Conexão com o banco de dados
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        dialect: "mysql",
-        port: process.env.DB_PORT
-    }
-)
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+});
 
 module.exports = {
     Sequelize: Sequelize,

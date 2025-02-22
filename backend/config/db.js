d6db67b37ddfef22e843eb5a827b5ba1da692377
Marcelo <marcelomarquesribeiro1@gmail.com>
2025-02-22 17:37:15 -0300

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,12 @@
 const Sequelize = require('sequelize')
 require("dotenv").config()
-const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+// Conexão com o banco de dados
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-});
-
-connection.connect((err) => {
-    if (err) {
-        console.error('Erro ao conectar ao banco:', err);
-    } else {
-        console.log('Conexão bem-sucedida ao MySQL!');
-    }
-});
+    dialect: "mysql",
+})
 
 module.exports = {
     Sequelize: Sequelize,

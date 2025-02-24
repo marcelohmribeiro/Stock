62ad4740e32ed4cd2a550ddb4f15f06227815fac
marcelohmribeiro <marcelomarquesribeiro1@gmail.com>
2025-02-24 18:50:25 -0300

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 // Conexão com o banco de dados
 const sequelize = new Sequelize({
-    host: process.env.DB_HOST,
+    // host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,

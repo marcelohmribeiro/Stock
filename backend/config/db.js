72ed022d3e7349df40223c6cad408a98d4851d01
marcelohmribeiro <marcelomarquesribeiro1@gmail.com>
2025-02-25 11:13:24 -0300

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,13 @@ const Sequelize = require('sequelize')
 require("dotenv").config()
 
 // Conexão com o banco de dados
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "mysql",
+const sequelize = new Sequelize({
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
 })
 
 sequelize.authenticate()

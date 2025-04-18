e5c3d3a3e3573c661ce389d6ff364b716697333b
marcelohmribeiro <marcelomarquesribeiro1@gmail.com>
2025-04-18 17:35:25 -0300

    ajuste

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,21 @@
 const { Sequelize } = require('sequelize')
 require("dotenv").config()
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // necessário com Neon
-    },
-  },
-})
+// Conexão com o banco de dados
+const sequelize = new Sequelize(
+    process.env.PGDATABASE,
+    process.env.PGUSER,
+    process.env.PGPASSWORD,
+    {
+        host: process.env.PGHOST,
+        port: process.env.PGPORT,
+        dialect: 'postgres',
+    }
+)
 
 sequelize.authenticate()
-  .then(() => console.log("Conectado ao banco PostgreSQL via Neon"))
-  .catch(err => console.error("Erro ao conectar:", err))
+    .then(() => console.log("Conectado ao banco PostgreSQL via Neon"))
+    .catch(err => console.error("Erro ao conectar:", err))
 
 module.exports = {
     Sequelize: Sequelize,
